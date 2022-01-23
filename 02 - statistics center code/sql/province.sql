54a5b3a3799535c49faa690e722dca5968f78afa
Mostafa Zebardast <z.mostafa3637@gmail.com>
2022-01-23 22:20:20 +0330

    Update Files

---
+++ 02 - statistics center code/sql/province.sql
@@ -0,0 +1,37 @@
+CREATE TABLE `province` (
+  `id` int(10) UNSIGNED NOT NULL PRIMARY KEY,
+  `name` varchar(22) NOT NULL,
+  `slug` varchar(24) NOT NULL
+);
+
+INSERT INTO `province`(id,name,slug) VALUES (0,'مرکزی', 'مرکزی');
+INSERT INTO `province`(id,name,slug) VALUES (1,'گیلان', 'گیلان');
+INSERT INTO `province`(id,name,slug) VALUES (2,'مازندران', 'مازندران');
+INSERT INTO `province`(id,name,slug) VALUES (3,'آذربایجان شرقی', 'آذربایجان-شرقی');
+INSERT INTO `province`(id,name,slug) VALUES (4,'آذربایجان غربی', 'آذربایجان-غربی');
+INSERT INTO `province`(id,name,slug) VALUES (5,'کرمانشاه', 'کرمانشاه');
+INSERT INTO `province`(id,name,slug) VALUES (6,'خوزستان', 'خوزستان');
+INSERT INTO `province`(id,name,slug) VALUES (7,'فارس', 'فارس');
+INSERT INTO `province`(id,name,slug) VALUES (8,'کرمان', 'کرمان');
+INSERT INTO `province`(id,name,slug) VALUES (9,'خراسان رضوی', 'خراسان-رضوی');
+INSERT INTO `province`(id,name,slug) VALUES (10,'اصفهان', 'اصفهان');
+INSERT INTO `province`(id,name,slug) VALUES (11,'سیستان و بلوچستان', 'سیستان-بلوچستان');
+INSERT INTO `province`(id,name,slug) VALUES (12,'کردستان', 'کردستان');
+INSERT INTO `province`(id,name,slug) VALUES (13,'همدان', 'همدان');
+INSERT INTO `province`(id,name,slug) VALUES (14,'چهارمحال و بختیاری', 'چهارمحال-بختیاری');
+INSERT INTO `province`(id,name,slug) VALUES (15,'لرستان', 'لرستان');
+INSERT INTO `province`(id,name,slug) VALUES (16,'ایلام', 'ایلام');
+INSERT INTO `province`(id,name,slug) VALUES (17,'کهگیلویه و بویر احمد', 'کهگیلویه-بویر-احمد');
+INSERT INTO `province`(id,name,slug) VALUES (18,'بوشهر', 'بوشهر');
+INSERT INTO `province`(id,name,slug) VALUES (19,'زنجان', 'زنجان');
+INSERT INTO `province`(id,name,slug) VALUES (20,'سمنان', 'سمنان');
+INSERT INTO `province`(id,name,slug) VALUES (21,'یزد', 'یزد');
+INSERT INTO `province`(id,name,slug) VALUES (22,'هرمزگان', 'هرمزگان');
+INSERT INTO `province`(id,name,slug) VALUES (23,'تهران', 'تهران');
+INSERT INTO `province`(id,name,slug) VALUES (24,'اردبیل', 'اردبیل');
+INSERT INTO `province`(id,name,slug) VALUES (25,'قم', 'قم');
+INSERT INTO `province`(id,name,slug) VALUES (26,'قزوین', 'قزوین');
+INSERT INTO `province`(id,name,slug) VALUES (27,'گلستان', 'گلستان');
+INSERT INTO `province`(id,name,slug) VALUES (28,'خراسان شمالی', 'خراسان-شمالی');
+INSERT INTO `province`(id,name,slug) VALUES (29,'خراسان جنوبی', 'خراسان-جنوبی');
+INSERT INTO `province`(id,name,slug) VALUES (30,'البرز', 'البرز');
